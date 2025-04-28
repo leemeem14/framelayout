c5c38a6174075a29b30ad59d5c8de5d5092516ef
leemeem14 <leemeem19@naver.com>
2025-04-28 15:29:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kopo/framelayou/MainActivity.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.framelayou;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linearspring, linearsummer, linearfall, linearwinter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        linearspring = findViewById(R.id.linear_spring);
+        linearsummer = findViewById(R.id.linear_summer);
+        linearfall = findViewById(R.id.linear_fall);
+        linearwinter = findViewById(R.id.linear_winter);
+    }
+}
