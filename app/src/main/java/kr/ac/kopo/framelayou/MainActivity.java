4919211ee73cb05725ec94e0b4e22a5683231987
leemeem14 <leemeem19@naver.com>
2025-04-28 15:41:03 +0900

    FrameLayout연습

--- app/src/main/java/kr/ac/kopo/framelayou/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/framelayou/MainActivity.java
@@ -1,6 +1,8 @@
 package kr.ac.kopo.framelayou;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout linearspring, linearsummer, linearfall, linearwinter;
+    Button btnSpring, btnSummer, btnFall, btnWinter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
         linearsummer = findViewById(R.id.linear_summer);
         linearfall = findViewById(R.id.linear_fall);
         linearwinter = findViewById(R.id.linear_winter);
+
+        btnSpring = findViewById(R.id.spring);
+        btnSummer = findViewById(R.id.summer);
+        btnFall = findViewById(R.id.fall);
+        btnWinter = findViewById(R.id.winter);
+
+        btnSpring.setOnClickListener(btnListener);
+        btnSummer.setOnClickListener(btnListener);
+        btnFall.setOnClickListener(btnListener);
+        btnWinter.setOnClickListener(btnListener);
+
     }
+    View.OnClickListener btnListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            linearspring.setVisibility(View.INVISIBLE);
+            linearsummer.setVisibility(View.INVISIBLE);
+            linearfall.setVisibility(View.INVISIBLE);
+            linearwinter.setVisibility(View.INVISIBLE);
+            Button btnE = (Button)v;
+            if(btnE == btnSpring){
+                linearspring.setVisibility(View.VISIBLE);
+            } else if(btnE == btnSummer){
+                linearsummer.setVisibility(View.VISIBLE);
+            } else if(btnE == btnFall){
+                linearfall.setVisibility(View.VISIBLE);
+            } else {
+                linearwinter.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 }
